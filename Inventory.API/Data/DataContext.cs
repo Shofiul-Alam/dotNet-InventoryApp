81a93a31bc02fe4ce1bdf9eba64a1cbde7a6ba4e
Shofiul alam <shofi.tafe@gmail.com>
2018-07-27 23:36:13 +1000

    Initial Commit

---
+++ Inventory.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Inventory.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
