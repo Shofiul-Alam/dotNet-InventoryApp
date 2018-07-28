8086fca1b7b87f546bc3a81221d70b4fb53b55e7
Shofiul alam <shofi.tafe@gmail.com>
2018-07-28 11:31:10 +1000

    Client Side Registration and Authentication

--- Inventory.API/Controllers/AuthController.cs
+++ Inventory.API/Controllers/AuthController.cs
@@ -1,18 +1,27 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Inventory.API.Data;
 using Inventory.API.Dtos;
 using Inventory.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Inventory.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController: ControllerBase
+    public class AuthController : ControllerBase
     {
         public IAuthRepository _repo { get; set; }
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
 
         }
@@ -25,7 +34,7 @@ namespace Inventory.API.Controllers
 
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
-            if(await _repo.userExists(userForRegisterDto.Username))
+            if (await _repo.userExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
             var userToCreate = new User
@@ -37,6 +46,47 @@ namespace Inventory.API.Controllers
 
             return StatusCode(201);
 
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            //Check Database to find user  
+            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
+
+            if (userFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            //Initialize Payload of Token authentication
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            //Signature 
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //Construct the Token authentication
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            //Creating token handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok( new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
--- Inventory.API/Data/AuthRepository.cs
+++ Inventory.API/Data/AuthRepository.cs
@@ -32,7 +32,7 @@ namespace Inventory.API.Data
                 {
                     var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                     for (int i = 0; i < computedHash.Length; i++) {
-                        if(computedHash[i] == user.PasswordHash[i]) return false;
+                        if(computedHash[i] != user.PasswordHash[i]) return false;
                     }
                 }
 
