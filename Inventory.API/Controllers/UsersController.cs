9e43fd6486b91ea9c5538ba5fa3c8d2420db643c
Shofiul alam <shofi.tafe@gmail.com>
2018-07-28 20:46:25 +1000

    Finished Extended Controller with Helpers, Dto Automapper etc.

---
+++ Inventory.API/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Inventory.API.Data;
+using Inventory.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _repo;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            var userToReturn = _mapper.Map<UserForDetailsDto>(user);
+            return Ok(userToReturn);
+        }
+    }
+}
